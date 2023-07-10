5c372e4b65d4c6f148f4f389b26be5d0bc2c8fa5
ValeriiLindenPy <115076024+ValeriiLindenPy@users.noreply.github.com>
2023-07-10 18:32:18 +0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,51 @@
+from aiogram import Bot, Dispatcher, executor, types
+import sqlite3
+import emoji
+
+# Connect to the SQLite3 database
+conn = sqlite3.connect('filmsdb.db')
+cursor = conn.cursor()
+
+
+def get_random_film() -> dict:
+    """Retrieve a random film from the database."""
+    cursor.execute("SELECT * FROM films ORDER BY RANDOM() LIMIT 1;")
+    film = cursor.fetchone()
+    if film:
+        # Map the fields to a dictionary for easier access
+        film_data = {
+            'title': film[0],
+            'description': film[1],
+            'rating': film[2],
+            'genre': film[3],
+            'runtime': film[4],
+            'year': film[5],
+            'image': film[6]
+        }
+        return film_data
+    return None
+
+
+bot = Bot("5628106360:AAFMhRgtwbEAo02yHg0kMHY6oORV0nJGfMk")
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    film = get_random_film()
+    title = film['title']
+    image = film['image']
+    description = film['description']
+    rating = film['rating']
+    genre = film['genre']
+    runtime = film['runtime']
+    year = film['year']
+    emj = emoji.emojize(":film_frames:")
+    post = f"""{emj} <b>{title}</b>\n<i>{genre}</i>\n\n{description}\n\n<b>Year:</b> {year}\n<b>Imdb:</b> {rating}\n<b>Runtime:</b> {runtime}
+    """
+    await bot.send_photo(message.chat.id, image, caption=post,  parse_mode="HTML")
+
+
+# Start the bot
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
